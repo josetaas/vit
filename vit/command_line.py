8c531fc9657b49a860c21211025e0844acba95e6
Jose Francisco Taas <josetaas@gmail.com>
2020-12-05 17:50:30 +0800

    Adds handler for Ctrl+C to avoid closing the application.

--- vit/command_line.py
+++ vit/command_line.py
@@ -1,8 +1,18 @@
+import signal
 from vit import parse_options, Application
+
+
+def handler(signum, frame):
+    pass
+
+
+signal.signal(signal.SIGINT, handler)
+
 
 def main():
     options, filters = parse_options()
     Application(options, filters)
+
 
 if __name__ == '__main__':
     main()
